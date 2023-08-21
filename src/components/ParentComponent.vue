cd6586112ec40b58b86bcddd86fe48e8cba3eabe
ahakbsl <ahak.bsl@gmail.com>
2023-08-22 01:18:58 +0600

    tailwind added in npm

---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+import ChildComponent from "./ChildComponent.vue";
+const props = defineProps(["message"]);
+const messageFromChild = ref("");
+
+</script>
+
+<template>
+  <div class="p-10 border-8 rounded-3xl border-black/50 w-[650px]">
+    <h2 class="mb-5 text-4xl font-semibold">Parrent Component</h2>
+    <h1 class="mb-5 text-lg font-bold">{{ messageFromChild }}</h1>
+    <ChildComponent :message="props.message" v-model:messageFromChild="messageFromChild" />
+  </div>
+</template>
+
+<style scoped></style>
